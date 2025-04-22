b98b6494fb54c2865361f37bdef4311b366dfd6f
Tamerlan <timurismeev@gmail.com>
2025-04-22 06:32:55 +0300

    polling educatiom

--- gitmember.py
+++ gitmember.py
@@ -1,32 +1,27 @@
 import requests
 import time
 
-API_CATS_URL = 'https://api.thecatapi.com/v1/images/search'
-API_URL = 'https://api.telegram.org/bot'
-BOT_TOKEN = '7718458852:AAEXau4ud0EvDmHa6jXC5z-vtG1ynGzmyZ0'
-ERROR_TEXT = 'Ура! Классный апдейт!'
-MAX_COUNTER = 100
 
+API_URL = 'https://api.telegram.org/bot'
+BOT_TOKEN = '5424991242:AAGwomxQz1p46bRi_2m3V7kvJlt5RjK9xr0'
 
 offset = -2
-counter = 0
-chat_id: int
+updates: dict
 
 
-while counter < MAX_COUNTER:
+def do_something() -> None:
+    print('Был апдейт')
 
-    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
 
+while True: 
+    start_time = time.time()
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
-            chat_id = result['message']['from']['id']
-            cat = requests.get(API_CATS_URL)
-            if cat.status_code==200:
-                cat_link = cat.json()[0]['url']
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
-            else: requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
-    time.sleep(1)
-    counter += 1
+            do_something()
+
+    time.sleep(3)
+    end_time = time.time()
+    print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
